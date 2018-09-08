27bedfdb6125b0cdd344f333d30e2f8cfa71a93a
Adrien Janvier <janvier.adrien@gmail.com>
2018-09-08 10:14:50 +0200

    SceneController is now a singleton

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -5,10 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        DontDestroyOnLoad(gameObject);
-	}
+    private static SceneController _instance;
+
+    public static SceneController Instance {
+        get { return _instance; }
+    }
+
+    void Awake()
+    {
+        if (_instance != null && _instance != this) 
+        {
+            Destroy(this.gameObject);
+        } else 
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
 	
 	// Update is called once per frame
 	void Update () {
