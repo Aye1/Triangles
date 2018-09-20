14f85a7627c56ad2a4bc3bae6d7780cb08f1e52c
Adrien Janvier <janvier.adrien@gmail.com>
2018-09-20 19:31:12 +0200

    Localization manager loading from CSV$

--- Assets/Scripts/StringLocalizationManager.cs
+++ Assets/Scripts/StringLocalizationManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,9 +19,12 @@ public class StringLocalizationManager : MonoBehaviour {
 
     private void Awake()
     {
-        if(_instance != null && _instance != this) {
+        if (_instance != null && _instance != this)
+        {
             Destroy(this.gameObject);
-        } else {
+        }
+        else
+        {
             _instance = this;
         }
         DontDestroyOnLoad(gameObject);
@@ -36,10 +40,11 @@ public class StringLocalizationManager : MonoBehaviour {
     }
 
     private void LoadLocalizedStrings() {
-        _allLocalizedStrings = new Dictionary<string, Dictionary<string, string>>();
+        /*_allLocalizedStrings = new Dictionary<string, Dictionary<string, string>>();
         foreach(string locale in GetLocalesList()) {
             _allLocalizedStrings.Add(locale, LoadOneLocale(locale));
-        }
+        }*/
+        _allLocalizedStrings = LoadDicoFromCSV();
         isReady = true;
     }
 
@@ -56,6 +61,9 @@ public class StringLocalizationManager : MonoBehaviour {
 
     public string GetLocString(string key, string locale) 
     {
+        if(!isReady) {
+            return null;
+        }
         if(_allLocalizedStrings.ContainsKey(locale)) {
             if(_allLocalizedStrings[locale].ContainsKey(key)) {
                 return _allLocalizedStrings[locale][key];
@@ -63,4 +71,55 @@ public class StringLocalizationManager : MonoBehaviour {
         }
         return _stringNotFound;
     }
+
+    private Dictionary<string, Dictionary<string, string>> LoadDicoFromCSV() {
+        Dictionary<string, Dictionary<string, string>> res = new Dictionary<string, Dictionary<string, string>>();
+        string[] rawLines = GetRawCSVLines();
+        string[][] splitLines = CleanedLines(RawToSplitLines(rawLines));
+        string[] keys = splitLines[0];
+        for (int i = 1; i < keys.Length; i++)
+        {
+            if (keys[i] != "")
+            {
+                Dictionary<string, string> newLocaleDic = new Dictionary<string, string>();
+                for (int j = 1; j < splitLines.Length;j++) {
+                    newLocaleDic.Add(splitLines[j][0], splitLines[j][i]);
+                }
+                res.Add(keys[i], newLocaleDic);
+            }
+        }
+        return res;
+    }
+
+    private string[] GetRawCSVLines() {
+        string basePath = Application.dataPath;
+        string completePath = basePath + "/Localization/Resources/Localizations.csv";
+        if (File.Exists(completePath))
+        {
+            string[] rawText = File.ReadAllLines(completePath);
+            return rawText;
+        }
+        return new string[]{};
+    }
+
+    private string[][] RawToSplitLines(string[] rawLines) {
+        char separator = ';';
+        string[][] splitLines = new string[rawLines.Length][];
+
+        for (int i = 0; i < rawLines.Length; i++) {
+            splitLines[i] = rawLines[i].Split(separator);
+        }
+        return splitLines;
+    }
+
+    private string[][] CleanedLines(string[][]splitLines) {
+        List<string[]> listLines = new List<string[]>();
+        foreach (string[] splitLine in splitLines) {
+            if(splitLine[0] != "") {
+                listLines.Add(splitLine);
+            }
+        }
+        return listLines.ToArray();
+    }
+    
 }
