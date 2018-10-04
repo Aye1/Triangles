41199926da5b824d18fb85fe684afd743a673849
Adrien Janvier <janvier.adrien@gmail.com>
2018-10-04 13:49:14 +0200

    Basic quest management

---
+++ Assets/Quest.cs
@@ -0,0 +1,9 @@
+﻿public abstract class Quest {
+
+    public string descriptionLocKey;
+    public string defaultDescription;
+
+    public abstract bool IsQuestCompleted();
+
+    public abstract string SpecificInformation();
+}
