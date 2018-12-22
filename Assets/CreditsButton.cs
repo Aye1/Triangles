469b08e4398ea9e0217625acdb4725b89442e660
Adrien Janvier <janvier.adrien@gmail.com>
2018-12-22 18:03:26 +0100

    Credits + fix localization file

---
+++ Assets/CreditsButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class CreditsButton : MonoBehaviour {
+
+
+	public Popup creditsPopup;
+
+	//use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(OpenPopup);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OpenPopup() {
+        UIHelper.DisplayGameObject(creditsPopup.gameObject);
+	}
+}
--- Assets/Scripts/Localization/LocalizationManager.cs
+++ Assets/Scripts/Localization/LocalizationManager.cs
@@ -2,8 +2,10 @@
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
-public class LocalizationManager : MonoBehaviour {
+public class LocalizationManager : MonoBehaviour
+{
 
 
     private Dictionary<string, Dictionary<string, string>> _allLocalizedStrings;
@@ -12,11 +14,16 @@ public class LocalizationManager : MonoBehaviour {
 
     public bool isReady = false;
 
-    public static LocalizationManager Instance 
+    public static LocalizationManager Instance
     {
         get { return _instance; }
     }
 
+    public Dictionary<string, Dictionary<string, string>> AllLocalizedStrings {
+        get {
+            return _allLocalizedStrings;
+        }
+    }
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -31,7 +38,7 @@ public class LocalizationManager : MonoBehaviour {
         LoadLocalizedStrings();
     }
 
-    private IEnumerable<string> GetLocalesList() 
+    private IEnumerable<string> GetLocalesList()
     {
         List<string> locales = new List<string>();
         locales.Add(Locales.en_GB);
@@ -39,29 +46,35 @@ public class LocalizationManager : MonoBehaviour {
         return locales;
     }
 
-    private void LoadLocalizedStrings() {
+    private void LoadLocalizedStrings()
+    {
         _allLocalizedStrings = LoadDicoFromCSV();
         isReady = true;
     }
 
-    public string GetLocString(string key, string locale) 
+    public string GetLocString(string key, string locale)
     {
-        if(!isReady) {
+        if (!isReady)
+        {
             return null;
         }
-        if(_allLocalizedStrings.ContainsKey(locale)) {
-            if(_allLocalizedStrings[locale].ContainsKey(key)) {
+        if (_allLocalizedStrings.ContainsKey(locale))
+        {
+            if (_allLocalizedStrings[locale].ContainsKey(key))
+            {
                 return _allLocalizedStrings[locale][key];
             }
         }
         return _stringNotFound;
     }
 
-    public string GetLocString(string key, SystemLanguage language) {
+    public string GetLocString(string key, SystemLanguage language)
+    {
         return GetLocString(key, SystemLanguageToString(language));
     }
 
-    private Dictionary<string, Dictionary<string, string>> LoadDicoFromCSV() {
+    private Dictionary<string, Dictionary<string, string>> LoadDicoFromCSV()
+    {
         Dictionary<string, Dictionary<string, string>> res = new Dictionary<string, Dictionary<string, string>>();
         string[] rawLines = GetRawCSVLines();
         string[][] splitLines = CleanedLines(RawToSplitLines(rawLines));
@@ -88,36 +101,46 @@ public class LocalizationManager : MonoBehaviour {
         return res;
     }
 
-    private string[] GetRawCSVLines() {
+    private string[] GetRawCSVLines()
+    {
         string basePath = Application.dataPath;
         TextAsset resObj = Resources.Load<TextAsset>("Localizations");
-        if(resObj != null) {
+        if (resObj != null)
+        {
             return RawToLines(resObj.text);
-        }  else {
+        }
+        else
+        {
             Debug.Log("Can't find localization file");
         }
-        return new string[]{};
+        return new string[] { };
     }
 
-    private string[] RawToLines(string rawText) {
+    private string[] RawToLines(string rawText)
+    {
         string[] separators = { "\n" };
         return rawText.Split(separators, System.StringSplitOptions.None);
     }
 
-    private string[][] RawToSplitLines(string[] rawLines) {
+    private string[][] RawToSplitLines(string[] rawLines)
+    {
         char separator = ';';
         string[][] splitLines = new string[rawLines.Length][];
 
-        for (int i = 0; i < rawLines.Length; i++) {
+        for (int i = 0; i < rawLines.Length; i++)
+        {
             splitLines[i] = rawLines[i].Split(separator);
         }
         return splitLines;
     }
 
-    private string[][] CleanedLines(string[][]splitLines) {
+    private string[][] CleanedLines(string[][] splitLines)
+    {
         List<string[]> listLines = new List<string[]>();
-        foreach (string[] splitLine in splitLines) {
-            if(splitLine[0] != "") {
+        foreach (string[] splitLine in splitLines)
+        {
+            if (splitLine[0] != "")
+            {
                 listLines.Add(splitLine);
             }
         }
@@ -136,14 +159,29 @@ public class LocalizationManager : MonoBehaviour {
         return Locales.en_GB;
     }
 
-    public static SystemLanguage StringToSystemLanguage(string language) {
-        if(language == Locales.fr_FR) {
+    public static SystemLanguage StringToSystemLanguage(string language)
+    {
+        if (language == Locales.fr_FR)
+        {
             return SystemLanguage.French;
-        } 
-        if (language == Locales.en_GB) {
+        }
+        if (language == Locales.en_GB)
+        {
             return SystemLanguage.English;
         }
         return SystemLanguage.English;
     }
+
+#if UNITY_EDITOR
+    [MenuItem("Debug/Dump available localizations")]
+    public static void DisplayAvailableLoc() {
+
+        foreach(Dictionary<string, string> locale in LocalizationManager.Instance.AllLocalizedStrings.Values) {
+            foreach(string value in locale.Values) {
+                Debug.Log(value);
+            }
+        }
+    }
+#endif
 
 }
